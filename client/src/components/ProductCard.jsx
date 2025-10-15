56a08ad883b10bf34faf8bc1dd61565912f4eddf
Khang Vu <49794277+khangvu0@users.noreply.github.com>
2025-10-14 22:29:44 -0400

    Created Product Card component

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import '../styles/ProductCard.css';
 
-const ProductCard = () => {
-    return <div>Product Card</div>;
-};
+export default function ProductCard({ product }) {
+    const { name, description, price, image, category } = product;
 
-export default ProductCard;
+    return (
+        <div className="product-card">
+            <img src={image} alt={name} className="product-image" />
+
+            <div className="product-details">
+                <h3 className="product-name">{name}</h3>
+                <p className="product-category">{category}</p>
+                <p className="product-description">{description}</p>
+                <p className="product-price">${price}</p>
+
+                <button className="add-to-cart-btn">Add to Cart</button>
+            </div>
+        </div>
+    );
+}
